7ac24d99502b58ca89c9c392a9f6e27e1bd2f62c
abhay.nirmal <abhayyy1912@gmail.com>
2024-04-15 01:12:17 +0530

    Modified register method

--- Handler/loginHandler.go
+++ Handler/loginHandler.go
@@ -29,7 +29,6 @@ func Login(c *gin.Context) {
 	userId = user.RefUserId
 	tokenString, err := util.CreateToken(userId)
 	if err != nil {
-		fmt.Printf(err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while CreateToken"})
 		return
 	}
@@ -45,20 +44,43 @@ func Register(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid data"})
 		return
 	}
-	err := dbConnection.DB.QueryRow("select RefUserId from RefUser Where username=?", user.Username).Scan(&userId)
+	err := dbConnection.DB.QueryRow("select RefUserId from RefUser Where username=$1", user.Username).Scan(&userId)
 	if err != nil {
-		if err != sql.ErrNoRows {
-			c.JSON(http.StatusCreated, gin.H{"message": "username is already exist!"})
-			return
+		if err == sql.ErrNoRows {
+			goto createToken
 		} else {
-			c.JSON(http.StatusCreated, gin.H{"message": "Error while execute query fro username is already exist or not"})
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Error while execute query from username is already exist or not"})
+			return
 		}
 	}
+	if userId != 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "username is already exist"})
+		return
+	}
+
+createToken:
 	tokenString, err := util.CreateToken(userId)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while CreateToken"})
 		return
 	}
 	user.Token = tokenString
-	c.JSON(http.StatusCreated, gin.H{"data": user})
+	//Need to add encypt password
+	fmt.Println(user.Password)
+
+	insertStmt := `insert into RefUser ("username", "userpassword","isadmin") values($1, $2, $3)`
+	res, e := dbConnection.DB.Exec(insertStmt, user.Username, user.Password, user.Is_Admin)
+	if e != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": e})
+		return
+	}
+	_, fail := res.RowsAffected()
+	if fail != nil {
+		fmt.Println(fail)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while Insert query for new user"})
+		return
+	} else {
+		c.JSON(http.StatusCreated, gin.H{"data": user})
+		return
+	}
 }
--- Models/models.go
+++ Models/models.go
@@ -3,7 +3,7 @@ package models
 type RefUser struct {
 	RefUserId uint   `json:"user_id"`
 	Username  string `json:"username"`
-	Password  string `json:"-"`
+	Password  string `json:"password"`
 	Token     string `json:"token"`
 	Is_Admin  bool   `json:"is_admin"`
 }
